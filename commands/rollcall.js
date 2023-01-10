058a62c465f60e11ba3edb6378e6e5ce0de0b6d2
Forrest McIntyre <forrest.mcintyre@gmail.com>
2023-01-10 00:33:45 -0800

    F** update rollcall to include buttons and an embed

--- commands/rollcall.js
+++ commands/rollcall.js
@@ -1,4 +1,6 @@
-const { SlashCommandBuilder } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { SlashCommandBuilder, ButtonStyle, ActionRowBuilder, ButtonBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -6,6 +8,42 @@ module.exports = {
 		.setDescription('Run a rollcall for this week\'s pinball match')
 		.setDefaultMemberPermissions('0'),
 	async execute(interaction) {
-		await interaction.reply('rollcall!');
+		const variablesJson = path.join('./', 'data', 'next-match.json');
+		let date;
+		let venue;
+		let team;
+		try {
+			const data = fs.readFileSync(variablesJson);
+			const thisWeek = JSON.parse(data);
+
+			date = thisWeek.date;
+			venue = thisWeek.venue;
+			team = thisWeek.team;
+		} catch (e) {
+			console.error(e);
+		}
+		const replyButtons = new ActionRowBuilder()
+			.addComponents(
+				new ButtonBuilder()
+					.setCustomId('rollcall-accept')
+					.setLabel('Blast some balls!')
+					.setEmoji('1059189786910408714')
+					.setStyle(ButtonStyle.Success),
+				new ButtonBuilder()
+					.setCustomId('rollcall-decline')
+					.setLabel('Find me a sub')
+					.setStyle(ButtonStyle.Danger),
+			);
+		const results = new EmbedBuilder()
+			.setColor('f0791e')
+			.setTitle(`Contras vs ${team}`)
+			.setDescription(`Monday Night Pinball, Week 1 \n ${date} at ${venue}`)
+			.setAuthor({ name: 'Coindexter Contras', iconURL: 'https://i.imgur.com/wS0ZY6f.png' })
+			.setURL('https://www.mondaynightpinball.com/teams/CDC')
+			.addFields({ name: '\u200b', value: '\u200b' })
+			.setFooter({ text: 'This bot is brought to you by LuckBasedGaming', iconURL: 'https://i.imgur.com/f3E6fEN.png' })
+			.setThumbnail('https://i.imgur.com/V9kalvC.png');
+
+		await interaction.reply({ content: '@everyone It is that time again!', embeds: [results], components: [replyButtons] });
 	},
 };
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,4 +1,4 @@
-const { Events } = require('discord.js');
+const { Events, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	name: Events.InteractionCreate,
@@ -17,6 +17,17 @@ module.exports = {
 				console.error(`Error executing ${interaction.commandName}`);
 				console.error(error);
 			}
+		} else if (interaction.isButton() && interaction.message.embeds[0]) {
+			const embed = interaction.message.embeds[0];
+			let newEmbed;
+			if (interaction.customId === 'rollcall-accept') {
+				newEmbed = EmbedBuilder.from(embed).addFields({ name: interaction.member.nickname, value: 'is in!' });
+				await interaction.reply({ content: 'You are in!', ephemeral: true });
+			} else if (interaction.customId === 'rollcall-decline') {
+				newEmbed = EmbedBuilder.from(embed).addFields({ name: interaction.member.nickname, value: 'needs a sub!' });
+				await interaction.reply({ content: 'We will find you a sub :smile:', ephemeral: true });
+			}
+			await interaction.message.edit({ embeds: [newEmbed] });
 		} else if (interaction.isAutocomplete()) {
 			const command = interaction.client.commands.get(interaction.commandName);
 
