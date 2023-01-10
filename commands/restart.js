8051c20eeddaf0e592bbcb2055f96abc62f973e4
Forrest McIntyre <forrest.mcintyre@gmail.com>
2023-01-10 00:51:47 -0800

    F** add a restart command

---
+++ commands/restart.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('restart')
+		.setDescription('Restart the bot')
+		.setDefaultMemberPermissions(0),
+	async execute(interaction) {
+		await interaction.reply({ content: 'Restarting...', ephemeral: true });
+		process.exit();
+	},
+};
