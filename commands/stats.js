d4c70f6bc504dd811670d56d46242ab08b8079d5
Forrest McIntyre <forrest.mcintyre@gmail.com>
2023-01-18 15:27:17 -0800

    F** add option fields for ifpa and matchplay ids and read those fields

--- commands/stats.js
+++ commands/stats.js
@@ -3,8 +3,19 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('stats')
-		.setDescription('Set/Get stats for user'),
+		.setDescription('Set/Get stats for user')
+		.addNumberOption(option =>
+			option
+				.setName('ifpa')
+				.setDescription('Your IFPA ID'))
+		.addNumberOption(option =>
+			option
+				.setName('match-play')
+				.setDescription('Your matchplay ID')),
 	async execute(interaction) {
-		await interaction.reply('hello world');
+		const user = interaction.user;
+		const ifpa = interaction.options.getNumber('ifpa');
+		const matchPlay = interaction.options.getNumber('match-play');
+		await interaction.reply(`${user.username}'s IFPA ID has been updated to ${ifpa} and MatchPlay ID has been updated to ${matchPlay}`);
 	},
 };
