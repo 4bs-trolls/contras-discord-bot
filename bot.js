614b81446809c974e3bfcad27f6636402eaa9eed
Forrest McIntyre <forrest.mcintyre@gmail.com>
2023-03-04 23:47:39 -0800

    F** Update bot.js to use dotenv for env vars

--- bot.js
+++ bot.js
@@ -7,6 +7,7 @@ dotenv.config();
 
 // Require the necessary discord.js classes
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const token = process.env.TOKEN;
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -41,4 +42,4 @@ for (const file of eventFiles) {
 }
 
 // Log in to Discord with your client's token
-client.login(process.env.TOKEN);
+client.login(token);
