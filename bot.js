8f0f7ab39001175b03f32af4fcb40161fd0fdac9
Forrest McIntyre <forrest.mcintyre@gmail.com>
2023-03-04 23:37:56 -0800

    F** Setup dotenv

--- bot.js
+++ bot.js
@@ -1,9 +1,12 @@
 const fs = require('node:fs');
 const path = require('node:path');
 
+// load .env file
+const dotenv = require('dotenv');
+dotenv.config();
+
 // Require the necessary discord.js classes
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -38,4 +41,4 @@ for (const file of eventFiles) {
 }
 
 // Log in to Discord with your client's token
-client.login(token);
+client.login(process.env.TOKEN);
