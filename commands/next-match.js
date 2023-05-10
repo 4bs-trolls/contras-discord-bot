1850dbf468065609b6fc0309149044f6ca0af749
Forrest McIntyre <forrest.mcintyre@gmail.com>
2023-05-09 20:56:53 -0700

    F** use new schedule table in supabase

--- commands/next-match.js
+++ commands/next-match.js
@@ -1,27 +1,40 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { SlashCommandBuilder } = require('discord.js');
+const {SlashCommandBuilder} = require('discord.js');
+const {createClient} = require('@supabase/supabase-js');
+
+const supabaseUrl = 'https://nwpgecjxpwvdwoczvuwr.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+// Create a single supabase client for interacting with your database
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('next-match')
-		.setDescription('View the upcoming match details'),
-	async execute(interaction) {
-		let message = '';
-		const variablesJson = path.join('./', 'data', 'next-match.json');
-		try {
-			const data = fs.readFileSync(variablesJson);
-			const thisWeek = JSON.parse(data);
+    data: new SlashCommandBuilder()
+        .setName('next-match')
+        .setDescription('View the upcoming match details'),
 
-			const date = thisWeek.date;
-			const venue = thisWeek.venue;
-			const team = thisWeek.team;
+    async execute(interaction) {
+        let message = '';
+        try {
+            let {data: upcomingMatches, error} = await supabase
+                .from('schedule')
+                .select('date, venue, venues(name), opponent, teams(name)')
+                .eq('season', 17) // TODO: Update this to be a constant
+                .gte('date', new Date().toDateString())
+                .order('date', {ascending: true})
+                .limit(1);
 
-			message = 'The upcoming match is:\n\n`Date:` *' + date + '* \n`Venue:` *' + venue + '* \n`Team:` *' + team + '*';
-		} catch (e) {
-			message = 'Failed to retrieve this week\'s data';
-		}
+            if (upcomingMatches.length === 0) {
+                message = 'There are no upcoming matches';
+            } else {
+                const date = new Date(upcomingMatches[0].date).toLocaleString('en-CA', {month: 'short', day: 'numeric'})
+                const venue = upcomingMatches[0].venues.name;
+                const team = upcomingMatches[0].teams.name;
 
-		await interaction.reply(message);
-	},
+                message = 'The upcoming match is:\n\n`Date:` *' + date + '* \n`Venue:` *' + venue + '* \n`Team:` *' + team + '*';
+            }
+        } catch (e) {
+            message = 'Failed to retrieve this week\'s data';
+        }
+
+        await interaction.reply(message);
+    },
 };
