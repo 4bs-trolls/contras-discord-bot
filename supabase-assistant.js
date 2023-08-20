5e568e7155fac78a359c08fd21a9e90b78862857
Forrest McIntyre <forrest.mcintyre@gmail.com>
2023-08-20 15:17:52 -0700

    F!! update rollcall to work with database

--- supabase-assistant.js
+++ supabase-assistant.js
@@ -9,7 +9,7 @@ const scheduleTableName = process.env.SCHEDULE_TABLE;
 async function getUpcomingMatch() {
     let {data: upcomingMatches, error} = await supabase
         .from(scheduleTableName)
-        .select('date, venue, venues(name), opponent, teams(name)')
+        .select('week, date, venue, venues(name), opponent, teams(name)')
         .eq('season', 18) // TODO: Update this to be a constant
         .gte('date', new Date().toDateString())
         .order('date', {ascending: true})
@@ -18,10 +18,11 @@ async function getUpcomingMatch() {
     if (upcomingMatches.length === 0) {
         return 'There are no upcoming matches';
     } else {
-        const date = new Date(upcomingMatches[0].date).toLocaleString('en-CA', {month: 'short', day: 'numeric'})
+        const week = upcomingMatches[0].week;
+        const date = new Date(upcomingMatches[0].date).toLocaleString('en-US', {timeZone: 'UTC', month: 'long', day: 'numeric'})
         const venue = upcomingMatches[0].venues.name;
         const team = upcomingMatches[0].teams.name;
-        return {date, venue, team};
+        return {week, date, venue, team};
     }
 }
 
