02e4f271d6bea6e81dc8046b559f031f19ed7b14
Forrest McIntyre <forrest.mcintyre@gmail.com>
2023-08-20 11:05:56 -0700

    r - refactor table name into env_vars

--- supabase-assistant.js
+++ supabase-assistant.js
@@ -3,11 +3,12 @@ const supabaseUrl = 'https://nwpgecjxpwvdwoczvuwr.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
 // Create a single supabase client for interacting with your database
 const supabase = createClient(supabaseUrl, supabaseKey)
+const scheduleTableName = 'schedule_development';
 
 
 async function getUpcomingMatch() {
     let {data: upcomingMatches, error} = await supabase
-        .from('schedule_development')
+        .from(scheduleTableName)
         .select('date, venue, venues(name), opponent, teams(name)')
         .eq('season', 17) // TODO: Update this to be a constant
         .gte('date', new Date().toDateString())
