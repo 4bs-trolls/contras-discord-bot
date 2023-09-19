e92d629fa105a1f07e9ba84b8729bb5acd656159
Forrest McIntyre <forrest.mcintyre@gmail.com>
2023-09-18 19:46:48 -0700

    F!! update subs to work with database

--- commands/subs.js
+++ commands/subs.js
@@ -1,5 +1,6 @@
-const { team, venue, date } = require('./../data/next-match.json');
 const { SlashCommandBuilder, ButtonStyle, ActionRowBuilder, ButtonBuilder } = require('discord.js');
+const path = require("path");
+const supabase = require( path.join(__dirname, '../supabase-assistant.js') )
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -7,10 +8,22 @@ module.exports = {
 		.setDescription('Run a sub rollcall for this week\'s pinball match')
 		.setDefaultMemberPermissions('0'),
 	async execute(interaction) {
-		const subsChannel = getSubsChannel(interaction);
-		const acceptButton = getReplyButton();
-		await interaction.reply({ content: `Subs requested for match against **${team}** at **${venue}** on **${date}**`, ephemeral: true });
-		await subsChannel.send({ content: `@here Someone is out this week on the normal roster so we could use your help! The upcoming match is on **${date}** at **${venue}** against **${team}** \n\nIf you would like to sub for the :contras: ontras this week, let us know by tapping the button below!`, components: [acceptButton] });
+		try {
+			const result = await supabase.getUpcomingMatch();
+			if (result === 'There are no upcoming matches') {
+				await interaction.reply({ content: 'There are no upcoming matches to rollcall for', ephemeral: true });
+			} else {
+				const {week, date, venue, team} = result;
+				const subsChannel = getSubsChannel(interaction);
+				const acceptButton = getReplyButton();
+				await interaction.reply({ content: `Subs requested for match against **${team}** at **${venue}** on **${date}**`, ephemeral: true });
+				await subsChannel.send({ content: `@here Someone is out this week on the normal roster so we could use your help! We are looking for subs for Week ${week} - **${date}** at **${venue}** against **${team}** \n\nIf you would like to sub for the Contras this week, let us know by tapping the button below!`, components: [acceptButton] });
+
+			}
+
+		} catch (e) {
+			await interaction.reply({ content: 'Failed to retrieve this week\'s data', ephemeral: true });
+		}
 	},
 };
 
